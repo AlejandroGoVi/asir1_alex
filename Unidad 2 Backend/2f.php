cdc07892c6cc1935e54cfeaa81aa573604f06194
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-10-07 12:49:43 +0200

    07/10/2019

---
+++ Unidad 2 Backend/2f.php
@@ -0,0 +1,27 @@
+ <?php
+ /*
+ $Dias = array('Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo');
+ echo "Hoy es ".$Dias[0]." y mañana sera ".$Dias[1];
+ */
+
+ $meses=[
+	'Enero',
+	'Febrero',
+	'Marzo',
+	'Abril',
+	'Mayo',
+	'Junio',
+	'Julio',
+	'Agosto',
+	'Septiembre',
+	'Octubre',
+	'Noviembre',
+	'Diciembre',
+	
+ ];
+for($i=0;$i<12;$i=$i+1)
+	echo $meses[$i];
+
+foreach($meses as $mes)
+	echo $mes;
+ ?>
