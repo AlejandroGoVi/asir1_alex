34716904214cc38cbcf5cc27e46a6aa5e8d7b7f2
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-10-14 11:06:12 +0200

    14/10/2019

--- Unidad 2 Backend/2f.php
+++ Unidad 2 Backend/2f.php
@@ -1,10 +1,11 @@
+
  <?php
  /*
  $Dias = array('Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo');
  echo "Hoy es ".$Dias[0]." y mañana sera ".$Dias[1];
  */
-
- $meses=[
+ {
+	 $meses=[
 	'Enero',
 	'Febrero',
 	'Marzo',
@@ -20,13 +21,27 @@
 	
  ];
 for($i=0;$i<12;$i=$i+1)
-	echo $meses[$i];
+	echo $meses[$i].'<br>';
 
 foreach($meses as $mes)
-	echo $mes;
-	
-$mes =
-array ('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
-
-echo "Estamos a". $mes[9];
+	echo $mes.'<br>';
+ }	
+ 
+ function mes($n){
+	$mes =
+	array  ('Enero',
+			'Febrero',
+			'Marzo',
+			'Abril',
+			'Mayo',
+			'Junio',
+			'Julio',
+			'Agosto',
+			'Septiembre',
+			'Octubre',
+			'Noviembre',
+			'Diciembre');
+	return "Estamos a ". $mes[$n-1].'</br>';
+ }
+ echo mes(1);
  ?>
