edbf55644da82fcc590aca64697a4759c49d55b6
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-11-13 08:53:27 +0100

    13/11/2019 8:53

--- Unidad 2 Backend/triangulos_a.php
+++ Unidad 2 Backend/triangulos_a.php
@@ -1,33 +1,20 @@
 <?php
-function tipo($a, $b, $c) {
-	$r='rectangulo';
-		if ($a>90 || $b>90 || $c>90) {
-			$r='obtusangulo';
-			}
-		
-		if($a<90 and $b<90 and $c<90){
-			$r='acutangulo';
-	}
-	return $r;
-}
 
 function tipo_lados($a,$b,$c){
-$r='escaleno';
-if($a==$b or $b==$c) $r='isosceles';
-if($a==$b and $b==$c) $r='equilátero';
+	$r='escaleno';
+	if($a==$b or $b==$c) 
+	{$r='isosceles';
+}
+	if($a==$b and $b==$c) 
+	{$r='equilátero';
+}
+
 echo $r;
 }
 
-echo tipo(30,90,60)."<br>";
-echo tipo(60,60,60)."<br>";
-echo tipo(30,30,120)."<br>";
+//Banco the test
 
-echo tipo_lados(1,1,1).'<br>';
-echo tipo_lados(1,1,2).'<br>';
-echo tipo_lados(2,1,1).'<br>';
-echo tipo_lados(1,2,1).'<br>';
-echo tipo_lados(2,1,1).'<br>';
-echo tipo_lados(2,2,2).'<br>';
-echo tipo_lados(3,2,1).'<br>';
-echo tipo_lados(4,4,4).'<br>';
-echo tipo_lados(0,0,0).'<br>';
+echo $r(1,1,2)."<br>";
+echo $r(1,2,2)."<br>";
+echo $r(2,1,2)."<br>";
+
