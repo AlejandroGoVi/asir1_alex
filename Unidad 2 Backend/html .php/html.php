ae800d8efc843431e8284145be9d4537a59b83eb
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-11-18 10:53:55 +0100

    18/11/2019 10:53

--- Unidad 2 Backend/html .php/html.php
+++ Unidad 2 Backend/html .php/html.php
@@ -4,7 +4,8 @@
 </head>
 
 <body>
-
+<body bgcolor="#E6E6FA">
+<body topmargin="100" leftmargin="0" marginheight="0" marginwidth="50">
 <h1>My Art Zone</h1> </div>
 <p><h2>This Zone is going to be only for art </h2></p>
 <img src="Img1.jpg" alt="Death of Julius Cesar" height="620" width="990 ">
@@ -13,8 +14,38 @@
 <p><a href="https://en.wikipedia.org/wiki/Vincenzo_Camuccini">Vincenzo Camuccini</a></p>
 
 <h3><b>Coming</b><u>soon</u></h3>
-<h1 style="color:blue;">Wow esto esta azul</h1>
+<font size="60" color="blue">Wow esto esta azul</font>
 <center>Asi de centrado quiero estar yo</center>
 <font size="60">Igual esto es mucho</font>
+ Texto de ejem<br>
+ plo 
+ <p>hcewopvweubwepbncqweipcbnksdcbnskkehicvndbvisdobvisbvisdobvsidvsvisdbvisdbv+</p>
+ <p>jvbhwjbhjvbhwiviwebvhiwebhviwbvweibvibweiviwebvweibvwibwiebviwebwioburq4890ytq4utnmuiq4utnm uivnunptuqqqqbtu ibviq4uqvvqibvuqiu</p>
+ 
+ <h2> Primera linea de la estructura</h2>
+ <h3>Segunda linea de la estructura</h3>
+ <h4>tercera linea de la estructura</h4>
+ <table border="1" style="width:100%">
+  <tr>
+    <th>Provincias bañadas por el cantabrico</th>
+  </tr>
+  <tr>
+    <td>Pais Vasco</td>
+  </tr>
+  <tr>
+    <td>Cantabria</td>
+  </tr>
+    <tr>
+    <td>Asturias</td>
+  </tr>
+  
+  <form>
+  Nombre:<br>
+  <input type="text" name="firstname"><br>
+  Apellidos:<br>
+  <input type="text" name="lastname">
+  <p><input type="submit" value="Submit"></p>
+</form>
+</table>
 </body>
 </html>
