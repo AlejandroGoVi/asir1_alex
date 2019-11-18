aee98c0ba775a0375c87e45dc43d30671a04f129
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-11-18 10:00:32 +0100

    18/11/2019 10:00

---
+++ Unidad 2 Backend/html .php/html.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>My Art Zone</title>
+</head>
+
+<body>
+
+<h1>My Art Zone</h1> </div>
+<p><h2>This Zone is going to be only for art </h2></p>
+<img src="Img1.jpg" alt="Death of Julius Cesar" height="620" width="990 ">
+<p><a href="https://en.wikipedia.org/wiki/The_Death_of_Julius_Caesar">Death of Julius Cesar</a></p>
+ - 1804 and 1805 - 
+<p><a href="https://en.wikipedia.org/wiki/Vincenzo_Camuccini">Vincenzo Camuccini</a></p>
+</body>
+</html>
