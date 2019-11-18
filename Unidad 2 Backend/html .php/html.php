a1c6046f648f411d86c23869787ad060c2a0465e
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-11-18 11:23:17 +0100

    18/11/2019 11:23

--- Unidad 2 Backend/html .php/html.php
+++ Unidad 2 Backend/html .php/html.php
@@ -3,9 +3,8 @@
 <title>My Art Zone</title>
 </head>
 
-<body>
-<body bgcolor="#E6E6FA">
-<body topmargin="100" leftmargin="0" marginheight="0" marginwidth="50">
+<body bgcolor="#E6E6FA" topmargin="100" leftmargin="0" marginheight="0" marginwidth="50">
+
 <h1>My Art Zone</h1> </div>
 <p><h2>This Zone is going to be only for art </h2></p>
 <img src="Img1.jpg" alt="Death of Julius Cesar" height="620" width="990 ">
@@ -22,10 +21,20 @@
  <p>hcewopvweubwepbncqweipcbnksdcbnskkehicvndbvisdobvisbvisdobvsidvsvisdbvisdbv+</p>
  <p>jvbhwjbhjvbhwiviwebvhiwebhviwbvweibvibweiviwebvweibvwibwiebviwebwioburq4890ytq4utnmuiq4utnm uivnunptuqqqqbtu ibviq4uqvvqibvuqiu</p>
  
- <h2> Primera linea de la estructura</h2>
- <h3>Segunda linea de la estructura</h3>
- <h4>tercera linea de la estructura</h4>
- <table border="1" style="width:100%">
+ <h2>Tipos de bebidas </h2>
+ <h3> Bebidas alcoholicas</h3>
+ <h4> Cerveza</h4>
+ <h4> Vino</h4>
+ <h4> Sidra</h4>
+ <h3> Bebidas para fiestas</h3>
+ <h4> Zumo de naranja</h4>
+ <h4> Batido de chocolate</h4>
+ <h4> Kas de limón</h4>
+ <h4> CocaCola</h4>
+ <h3>Bebidas para comida</h3>
+ <h4> Vino</h4>
+ <h4> Agua</h4>
+ <table border="1" >
   <tr>
     <th>Provincias bañadas por el cantabrico</th>
   </tr>
@@ -38,7 +47,7 @@
     <tr>
     <td>Asturias</td>
   </tr>
-  
+</table>
   <form>
   Nombre:<br>
   <input type="text" name="firstname"><br>
@@ -46,6 +55,5 @@
   <input type="text" name="lastname">
   <p><input type="submit" value="Submit"></p>
 </form>
-</table>
 </body>
 </html>
