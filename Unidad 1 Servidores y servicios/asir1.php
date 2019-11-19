cf3d0d64bf1ec812d58d8f269b6f1497eef92142
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-11-19 11:52:30 +0100

    19/11/2019 11:52

--- Unidad 1 Servidores y servicios/asir1.php
+++ Unidad 1 Servidores y servicios/asir1.php
@@ -1,9 +1,19 @@
 <html>
 <body>
+#mi ip 
+<a href=”192.168.0.57”>
+Bruno 
+<a href”192.168.0.55”>
 <form action="Datos.php" method="post">
 	Nombre: <input type="text" name="nombre"><br>
 	Email:  <input type="email" name="email"><br>
 	<input type="submit" value="Enviar">
+<?php
+$file = fopen("visitas.txt","a");
+$ip=$_SERVER['REMOTE_ADDR'];
+echo fwrite($file,$ip."\r\n");
+fclose($file);
+?>
 </form>
 </body>
 </html>
