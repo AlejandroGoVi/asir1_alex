e4a8d35eb4025bcfd375d06fc1c6db5da6fbd219
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-12-02 10:11:57 +0100

    02/12/2019 10:11

---
+++ Unidad 2 Backend/html .php/css.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<title>My Art Zone</title>
+</head>
+
+<body style="
+	background-color:#E6E6FA;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 50px;
+"
+>
+<!--  
+
+-->
+<h1 style>My Art Zone</h1> </div>
+<p><h2 style>This Zone is going to be only for art </h2></p>
+<img src="Img1.jpg" alt="Death of Julius Cesar" height="620" width="990 ">
+<p><a href="https://en.wikipedia.org/wiki/The_Death_of_Julius_Caesar">Death of Julius Cesar</a></p>
+ - 1804 and 1805 - 
+<p><a href="https://en.wikipedia.org/wiki/Vincenzo_Camuccini">Vincenzo Camuccini</a></p>
+
+<h3><b>Coming</b><u>soon</u></h3>
+
+
+<font style font-size:"60";
+			color="blue">Wow esto esta azul</font>
+			
+			
+<center>Asi de centrado quiero estar yo</center>
+<font size="60">Igual esto es mucho</font>
+ Texto de ejem<br>
+ plo 
+ <p>hcewopvweubwepbncqweipcbnksdcbnskkehicvndbvisdobvisbvisdobvsidvsvisdbvisdbv+</p>
+ <p>jvbhwjbhjvbhwiviwebvhiwebhviwbvweibvibweiviwebvweibvwibwiebviwebwioburq4890ytq4utnmuiq4utnm uivnunptuqqqqbtu ibviq4uqvvqibvuqiu</p>
+ 
+ <h2>Tipos de bebidas </h2>
+ <h3> Bebidas alcoholicas</h3>
+ <h4> Cerveza</h4>
+ <h4> Vino</h4>
+ <h4> Sidra</h4>
+ <h3> Bebidas para fiestas</h3>
+ <h4> Zumo de naranja</h4>
+ <h4> Batido de chocolate</h4>
+ <h4> Kas de limón</h4>
+ <h4> CocaCola</h4>
+ <h3>Bebidas para comida</h3>
+ <h4> Vino</h4>
+ <h4> Agua</h4>
+ <table border="1" >
+  <tr>
+    <th>Provincias bañadas por el cantabrico</th>
+  </tr>
+  <tr>
+    <td>Pais Vasco</td>
+  </tr>
+  <tr>
+    <td>Cantabria</td>
+  </tr>
+    <tr>
+    <td>Asturias</td>
+  </tr>
+</table>
+  <form>
+  Nombre:<br>
+  <input type="text" name="firstname"><br>
+  Apellidos:<br>
+  <input type="text" name="lastname">
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
