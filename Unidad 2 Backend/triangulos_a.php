c7c3ac013fed0c28577be2312258c7e83614622d
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-12-04 08:44:29 +0100

    03/12/2019 12:06

--- Unidad 2 Backend/triangulos_a.php
+++ Unidad 2 Backend/triangulos_a.php
@@ -10,8 +10,9 @@ function tipo_lados($a,$b,$c){
 return $r;
 }
 //Banco the test
-echo $r(1,1,2).Isosceles"<br>";
-echo $r(2,2,2).Equilatero"<br>";
-echo $r(1,2,2).Fallo"<br>";
-echo $r(2,1,2).Fallo"<br>";
+echo $r(1,1,2)." =Isosceles.<br>";
+echo $r(3,2,3)." =Isosceles.<br>";
+echo $r(2,2,2)." =Equilatero.<br>";
+echo $r(1,2,2)." =Fallo.<br>";
+echo $r(2,1,2)." =Fallo.<br>";
 ?>
