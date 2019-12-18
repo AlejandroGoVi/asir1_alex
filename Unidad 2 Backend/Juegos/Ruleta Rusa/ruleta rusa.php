26c7faaded763c3cd8d706ed38277cef2210d8b2
AlejandroGoVi <alejandrogovi16@gmail.com>
2019-12-18 09:59:48 +0100

    18/12/2019

--- Unidad 2 Backend/Juegos/Ruleta Rusa/ruleta rusa.php
+++ Unidad 2 Backend/Juegos/Ruleta Rusa/ruleta rusa.php
@@ -1,27 +1,20 @@
 <?php
 // opciones de ruleta
 $balas = [1,0,0,0,0,0];
-$balas2 = [0,1,0,0,0,0];
-$balas3 = [0,0,1,0,0,0];
-$balas4 = [0,0,0,1,0,0];
-$balas5 = [0,0,0,0,1,0];
-$balas6 = [0,0,0,0,0,1];
+
 //aleatorio
 shuffle($balas);
 
 print_r($balas);
 // Si estas muerto o no
 
-if ($balas == $balas) {
+if ($balas = [1]) {
 	
 	echo '<div style="font-size:3em">Has muerto!!</div>';
 }
-	
-elseif ($balas == $balas2,$balas3,$balas4,$balas5,$balas6) {
-	
+
+else {
 	echo '<div style="font-size:3em">Sigues vivo!!</div>';
 }
-
-
 
 ?>
